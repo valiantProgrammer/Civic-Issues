b5b4a7a31f4aff429e944854ef374b4f3f3d68bb
valiantProgrammer <rupayandey134@gmail.com>
2026-03-05 10:31:04 +0530

    1.0.3.1

--- app/user/components/components/Footer.js
+++ app/user/components/components/Footer.js
@@ -5,12 +5,16 @@ export default function Footer() {
     <footer className="bg-white border-t border-gray-200 py-3 fixed w-full z-50 bottom-0">
       <div className=" mx-auto px-3 sm:px-4 lg:px-8">
         <div className="text-center">
-          <Link
-            href="/help"
+          <button
+            onClick={() => {
+                setActiveSection('help');
+                onHelpClick();
+                setIsMenuOpen(false);
+              }}
             className="text-purple-600 hover:text-purple-700 font-medium transition-colors"
           >
             Need help?
-          </Link>
+          </button>
         </div>
       </div>
     </footer>
